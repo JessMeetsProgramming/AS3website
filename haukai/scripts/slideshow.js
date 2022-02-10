4376ace1fe32e8b8979673344843f08c50b08342
JessMeetsProgramming <jess16_greeny@hotmail.com>
2022-02-10 20:24:32 +1300

    attempt13

--- haukai/scripts/slideshow.js
+++ haukai/scripts/slideshow.js
@@ -1,48 +1,48 @@
+//run script when document has loaded
+$(document).ready(function() {
 
-    // if user resizes browser window - refresh the page so that the slideshow will stop and start depending on screen size
-    window.onresize = function() { 
-    location.reload(true); //reload from server instead of cache
-    continue; // breaks out of entire script?? change to return or timeout somehow        
-    }
+   
+    // if screen width is greater than 600px run the script
+    if (document.documentElement.clientWidth > 600) {
     
-    //run script when document has loaded
-    $(document).ready(function() {
+        // declare slideindex variable to count slides
+        var slideIndex = 0;
+        // run function
+        carousel();
 
-        // if screen width is greater than 600px run the script
-        if (document.documentElement.clientWidth > 600) {
-        
-            // declare slideindex variable to count slides
-            var slideIndex = 0;
-            // run function
-            carousel();
-
-            // function to chnage the image every 6 seconds
-            function carousel() {
-                // counter
-                var i;
-                // each slide
-                var x = document.getElementsByClassName("mySlides");
-                // if the number of slides is greater than 0 add 1 to counter (i)
-                for (i = 0; i < x.length; i++) {
-                    // change slide 1 display value to none
-                    x[i].style.display = "none";
-                
-                }
-                // increase value of slideIndex by 1
-                slideIndex++;
-                // if slide index is greater than the number of slides
-                if (slideIndex > x.length) {
-                    slideIndex = 1
-                }
-                // change slide display to block
-                x[slideIndex-1].style.display = "block";
-                setTimeout(carousel, 6000); // Change image every 6 seconds
-                
+        // function to chnage the image every 6 seconds
+        function carousel() {
+            // counter
+            var i;
+            // each slide
+            var x = document.getElementsByClassName("mySlides");
+            // if the number of slides is greater than 0 add 1 to counter (i)
+            for (i = 0; i < x.length; i++) {
+                // change slide 1 display value to none
+                x[i].style.display = "none";
+            
             }
-
+            // increase value of slideIndex by 1
+            slideIndex++;
+            // if slide index is greater than the number of slides
+            if (slideIndex > x.length) {
+                slideIndex = 1
+            }
+            // change slide display to block
+            x[slideIndex-1].style.display = "block";
+            setTimeout(carousel, 6000); // Change image every 6 seconds
+            
         }
-    });
 
+    }
+
+
+});   
+// if user resizes browser window - refresh the page so that the slideshow will stop and start depending on screen size
+window.onresize = function() { 
+    location.reload(true); //reload from server instead of cache
+    return;    // breaks out of entire script?? change to return or timeout somehow        
+}
 
 
 
